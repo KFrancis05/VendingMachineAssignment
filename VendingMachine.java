14e37359abbc6d5e7a30df388033024956bb5833
aditibaghel9 <asbaghel@quinnipiac.edu>
2025-02-12 19:46:14 -0500

    Welcome and Instructions

--- VendingMachine.java
+++ VendingMachine.java
@@ -2,5 +2,9 @@ public class VendingMachine {
     
     public static void main (String[] args){
 
+        System.out.println("Welcome to the Vending Machine!");
+        System.out.println("Here are the instructions to how the vending machine works!");
+        System.out.println("\n You can choose any snack you would like!\n Then you pick whichever snack you would like! \n Then you will be asked to insert money to pay for your snack. \n Then you will be asked for a confirmation. \n Finally! You can enjoy your snack! ");
+
     }
 }
